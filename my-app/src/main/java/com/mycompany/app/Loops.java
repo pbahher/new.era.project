b632039de072386aa2e9b53ccdd8f897109a41ff
Peter Bahher <pbahher@gmail.com>
2016-11-08 17:08:44 +0200

    all done

--- my-app/src/main/java/com/mycompany/app/Loops.java
+++ my-app/src/main/java/com/mycompany/app/Loops.java
@@ -3,33 +3,36 @@ package com.mycompany.app;
 public class Loops {
 
     public void outputtOddNumbers() {
-        for (int i = 1; i <= 99; i=i+2) {
-            System.out.print(i+" ");
+        for (int i = 1; i <= 99; i = i + 2) {
+            System.out.print(i + " ");
         }
         System.out.print("\n");
     }
 
     public void outputtNumbers() {
         int a = 1;
+
         while (a <= 99) {
-            System.out.print(a+" ");
-            a=a+2;
+            System.out.print(a + " ");
+            a = a + 2;
         }
     }
 
     public void countingFactorial() {
-        int n =1;
-        for (int a=1; a<=9; a=a+1) {
-            n=n*a;
+        int n = 1;
+
+        for (int a = 1; a <= 9; a = a + 1) {
+            n = n * a;
         }
-        System.out.println("\n"+n);
+        System.out.println("\n" + n);
     }
 
     public void countingFactorialNewMethod() {
-        int factorial=1;
-        int input=9;
-        while (input>0) {
-            factorial= factorial*input;
+        int factorial = 1;
+        int input = 9;
+
+        while (input > 0) {
+            factorial = factorial * input;
             input--;
         }
         System.out.print(factorial);
@@ -39,25 +42,26 @@ public class Loops {
         double x = 2;
         int n = 10;
         double c = 1;
+
         for (int i = 1; i <= n; i++) {
-            c= c * x;
+            c = c * x;
         }
-            System.out.println("\n"+(int)c);
+        System.out.println("\n" + (int) c);
     }
 
     public void calculateOfSequence() {
-        for (int a=0, i=1; i<=10; a=a-5, i++) {
-            System.out.print(a+" ");
+        for (int a = 0, i = 1; i <= 10; a = a - 5, i++) {
+            System.out.print(a + " ");
         }
         System.out.print("\n");
     }
 
     public void calculateOfSequenceNewMethod() {
-        for (int a=0, i=1; i<=10; a=a-5, i++) {
-            if (i==4) {
+        for (int a = 0, i = 1; i <= 10; a = a - 5, i++) {
+            if (i == 4) {
                 break;
             }
-            System.out.print(a+" ");
+            System.out.print(a + " ");
         }
     }
 }
--- my-app/src/main/java/com/mycompany/app/Variables.java
+++ my-app/src/main/java/com/mycompany/app/Variables.java
@@ -6,50 +6,55 @@ public class Variables {
     public void theNumberRemainder() {
         int q = 21;
         int w = 5;
-        System.out.println((q/w)+" остаток "+(q%w));
+
+        System.out.println((q / w) + " остаток " + (q % w));
     }
 
     public void sumOfNumbers() {
         int n = 22;
-        int a = (n%10);
-        int b = ((n/10)%10);
-        System.out.println("сумма чисел n = "+(a+b));
+        int a = (n % 10);
+        int b = ((n / 10) % 10);
+
+        System.out.println("сумма чисел n = " + (a + b));
     }
 
     public void roundingNumbers() {
-        double a =22.3;
+        double a = 22.3;
+
         System.out.println(Math.round(a));
     }
 
     public void roundingThreeNumbers() {
         int a = 345;
-        int b = (a%10);
-        int c = ((a/10)%10);
-        int d = ((a/100)%10);
-        System.out.println("сумма чисел a = "+(b+c+d));
+        int b = (a % 10);
+        int c = ((a / 10) % 10);
+        int d = ((a / 100) % 10);
+
+        System.out.println("сумма чисел a = " + (b + c + d));
     }
 
     public void calculateIncreasingSequence() {
         int a = 7;
         int b = 5;
         int c = 6;
-        if (a<b&&a<c) {
-            if (b<c)
-                System.out.println(a+" "+b+" "+c);
+
+        if (a < b && a < c) {
+            if (b < c)
+                System.out.println(a + " " + b + " " + c);
             else
-                System.out.println(a+" "+c+" "+b);
+                System.out.println(a + " " + c + " " + b);
         }
-            if (b<a&&b<c) {
-            if (a<c)
-                System.out.println(b+" "+a+" "+c);
+        if (b < a && b < c) {
+            if (a < c)
+                System.out.println(b + " " + a + " " + c);
             else
-                System.out.println(b+" "+c+" "+a);
+                System.out.println(b + " " + c + " " + a);
         }
-            if (c<b&&c<a) {
-            if (b<a)
-                System.out.println(c+" "+b+" "+a);
+        if (c < b && c < a) {
+            if (b < a)
+                System.out.println(c + " " + b + " " + a);
             else
-                System.out.println(c+" "+a+" "+b);
+                System.out.println(c + " " + a + " " + b);
         }
     }
 
