2ba1f492a87db0699dc1f1b2a71edfab3a6ac0ed
Peter Bahher <pbahher@gmail.com>
2016-11-08 17:43:08 +0200

    all done

--- my-app/src/test/java/com/mycompany/app/LoopsTest.java
+++ my-app/src/test/java/com/mycompany/app/LoopsTest.java
@@ -7,25 +7,39 @@ public class LoopsTest {
     static Loops loops = new Loops();
 
     @Test
-    public void outputtOddNumbers() { loops.outputtOddNumbers(); }
+    public void outputtOddNumbers() {
+        loops.outputtOddNumbers();
+    }
 
     @Test
-    public void outputtNumbers() { loops.outputtNumbers(); }
+    public void outputtNumbers() {
+        loops.outputtNumbers();
+    }
 
     @Test
-    public void countingFactorial() { loops.countingFactorial(); }
+    public void countingFactorial() {
+        loops.countingFactorial();
+    }
 
     @Test
-    public void countingFactorialNewMethod() { loops.countingFactorialNewMethod(); }
+    public void countingFactorialNewMethod() {
+        loops.countingFactorialNewMethod();
+    }
 
     @Test
-    public void calculateTheDegree() { loops.calculateTheDegree(); }
+    public void calculateTheDegree() {
+        loops.calculateTheDegree();
+    }
 
     @Test
-    public void calculateOfSequence() { loops.calculateOfSequence(); }
+    public void calculateOfSequence() {
+        loops.calculateOfSequence();
+    }
 
     @Test
-    public void calculateOfSequenceNewMethod() { loops.calculateOfSequenceNewMethod(); }
+    public void calculateOfSequenceNewMethod() {
+        loops.calculateOfSequenceNewMethod();
+    }
 
 
 }
--- my-app/src/test/java/com/mycompany/app/VariablesTest.java
+++ my-app/src/test/java/com/mycompany/app/VariablesTest.java
@@ -7,19 +7,29 @@ public class VariablesTest {
     static Variables variables = new Variables();
 
     @Test
-    public void theNumberRemainder() { variables.theNumberRemainder(); }
+    public void theNumberRemainder() {
+        variables.theNumberRemainder();
+    }
 
     @Test
-    public void sumOfNumbers() { variables.sumOfNumbers(); }
+    public void sumOfNumbers() {
+        variables.sumOfNumbers();
+    }
 
     @Test
-    public void roundingNumbers() { variables.roundingNumbers(); }
+    public void roundingNumbers() {
+        variables.roundingNumbers();
+    }
 
     @Test
-    public void roundingThreeNumbers() { variables.roundingThreeNumbers(); }
+    public void roundingThreeNumbers() {
+        variables.roundingThreeNumbers();
+    }
 
     @Test
-    public void calculateIncreasingSequence() { variables.calculateIncreasingSequence(); }
+    public void calculateIncreasingSequence() {
+        variables.calculateIncreasingSequence();
+    }
 
 
 }
