b8328505145d849c5b97d2615bb52cf888681927
Peter Bahher <pbahher@gmail.com>
2016-11-11 00:06:20 +0200

    all done

--- my-app/src/main/java/com/mycompany/app/Arrays.java
+++ my-app/src/main/java/com/mycompany/app/Arrays.java
@@ -2,9 +2,19 @@ package com.mycompany.app;
 
 public class Arrays {
 
-    /*public void findNull() {
+    public void findNull() {
 
-    }*/
+        int[] a = {2, 3, 0, 4, 0, 3, 0, 0};
+        int z = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == z) {
+
+                System.out.print(a[i] + " ");
+            }
+        }
+
+    }
 
     public void findMaxArrays() {
 
@@ -18,4 +28,35 @@ public class Arrays {
 
         System.out.println(max);
     }
+
+    public void lineSearch() {
+
+        int[] a = {2, 7, 1, 3, 0, 3, 0, 0};
+        int z = 3;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == z) {
+
+                System.out.println("\n" + i);
+                break;
+            }
+        }
+    }
+
+    public void maxElements() {
+
+        int[] a = {2, 5, 7, 2, 4, 3, 7};
+        int max = a[0];
+        int count = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (max < a[i])
+                max = a[i];
+
+
+        }
+
+        System.out.print(max);
+    }
+
 }
--- my-app/src/test/java/com/mycompany/app/ArraysTest.java
+++ my-app/src/test/java/com/mycompany/app/ArraysTest.java
@@ -6,13 +6,23 @@ public class ArraysTest {
 
     static Arrays arrays = new Arrays();
 
-    /*@Test
+    @Test
     public void findNull() {
         arrays.findNull();
-    }*/
+    }
 
     @Test
     public void findMaxArrays() {
         arrays.findMaxArrays();
+    }
+
+    @Test
+    public void lineSearch() {
+        arrays.lineSearch();
+    }
+
+    @Test
+    public void maxElements() {
+        arrays.maxElements();
     }
 }
