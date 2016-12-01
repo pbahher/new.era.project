c95c3171b1e2e601697b058cd8244896cc19c239
Peter Bahher <pbahher@gmail.com>
2016-12-01 15:34:19 +0200

    all done

--- my-app/src/main/java/com/mycompany/app/Collection.java
+++ my-app/src/main/java/com/mycompany/app/Collection.java
@@ -2,8 +2,107 @@ package com.mycompany.app;
 
 public class Collection {
 
-    public void zadacha01() {
+    public void trimToSize() {
 
+    }
+
+    public void ensureCapacity() {
+
+    }
+
+    public void size() {
+
+    }
+
+    public void isEmpty() {
+
+    }
+
+    public void contains() {
+
+    }
+
+    public void indexOff() {
+
+    }
+
+    public void lastIndexOff() {
+
+    }
+
+    public void clones() {
+
+    }
+
+    public void toArray() {
+
+    }
+
+    public void toArrayT() {
+
+    }
+
+    public void get() {
+
+    }
+
+    public void set() {
+
+    }
+
+    public void add() {
+
+    }
+
+    public void addPosition() {
+
+    }
+
+    public void removePosition() {
+
+    }
+
+    public void remove() {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public void addAll() {
+
+    }
+
+    public void addAllPosition() {
+
+    }
+
+    public void removeRange() {
+
+    }
+
+    public void removeAll() {
+
+    }
+
+    public void retainAll() {
+
+    }
+
+    public void listIteratorPosition() {
+
+    }
+
+    public void listIterator() {
+
+    }
+
+    public void iterator() {
+
+    }
+
+    public void subList() {
 
     }
 }
--- my-app/src/test/java/com/mycompany/app/CollectionTest.java
+++ my-app/src/test/java/com/mycompany/app/CollectionTest.java
@@ -7,8 +7,132 @@ public class CollectionTest {
     static Collection collection = new Collection();
 
     @Test
-    public void zadacha01() {
-        collection.zadacha01();
+    public void trimToSize() {
+        collection.trimToSize();
     }
 
+    @Test
+    public void ensureCapacity() {
+        collection.ensureCapacity();
+    }
+
+    @Test
+    public void size() {
+        collection.size();
+    }
+
+    @Test
+    public void isEmpty() {
+        collection.isEmpty();
+    }
+
+    @Test
+    public void contains() {
+        collection.contains();
+    }
+
+    @Test
+    public void indexOff() {
+        collection.indexOff();
+    }
+
+    @Test
+    public void lastIndexOff() {
+        collection.lastIndexOff();
+    }
+
+    @Test
+    public void clones() {
+        collection.clones();
+    }
+
+    @Test
+    public void toArray() {
+        collection.toArray();
+    }
+
+    @Test
+    public void toArrayT() {
+        collection.toArrayT();
+    }
+
+    @Test
+    public void get() {
+        collection.get();
+    }
+
+    @Test
+    public void set() {
+        collection.set();
+    }
+
+    @Test
+    public void add() {
+        collection.add();
+    }
+
+    @Test
+    public void addPosition() {
+        collection.addPosition();
+    }
+
+    @Test
+    public void removePosition() {
+        collection.removePosition();
+    }
+
+    @Test
+    public void remove() {
+        collection.remove();
+    }
+
+    @Test
+    public void clear() {
+        collection.clear();
+    }
+
+    @Test
+    public void addAll() {
+        collection.addAll();
+    }
+
+    @Test
+    public void addAllPosition() {
+        collection.addAllPosition();
+    }
+
+    @Test
+    public void removeRange() {
+        collection.removeRange();
+    }
+
+    @Test
+    public void removeAll() {
+        collection.removeAll();
+    }
+
+    @Test
+    public void retainAll() {
+        collection.retainAll();
+    }
+
+    @Test
+    public void listIteratorPosition() {
+        collection.listIteratorPosition();
+    }
+
+    @Test
+    public void listIterator() {
+        collection.listIterator();
+    }
+
+    @Test
+    public void iterator() {
+        collection.iterator();
+    }
+
+    @Test
+    public void subList() {
+        collection.subList();
+    }
 }
